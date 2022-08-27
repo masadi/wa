3bfb4b5a29133069b40adf823793c429e27aab89
masadi <masadi.com@gmail.com>
2022-08-27 21:30:39 +0700

    update

--- public/example.php
+++ public/example.php
@@ -1,84 +1,26 @@
 <?php
 
-namespace App\Http\Controllers;
+new ultraMsgChatBot("x2epn697nmzwnolg", "instance15890");
 
-use Illuminate\Http\Request;
-use UltraMsg\WhatsAppApi;
-use ultramsgDictionary;
-
-class WhatsappController extends Controller
+class ultraMsgChatBot
 {
-    public function index(){
-        $ultramsg_token="x2epn697nmzwnolg"; // Ultramsg.com token
-        $instance_id="instance15890"; // Ultramsg.com instance id
-        $client = new WhatsAppApi($ultramsg_token,$instance_id);
+    var $client;
+    public function __construct($ultramsg_token, $instance_id)
+    {
+        //incloud ultramsg class
+        //https://github.com/ultramsg/whatsapp-php-sdk
 
-        $to="6285231444789"; 
-        $body="Hello world"; 
-        $api=$client->sendChatMessage($to,$body);
-        dump($api);
+        //require_once ('vendor/autoload.php'); // if you use Composer 
+        //composer require ultramsg/whatsapp-php-sdk
 
-        $image="https://file-example.s3-accelerate.amazonaws.com/images/test.jpg"; 
-        $caption="image Caption"; 
-        $priority=10;
-        $referenceId="SDK";
-        $nocache=false; 
-        $api=$client->sendImageMessage($to,$image,$caption,$priority,$referenceId,$nocache);
-        dump($api);
+        require_once('ultramsg.class.php'); //Latest 2.0.3 stable
+        //Include a dictionary to generate random words and sentences
+        require_once('ultramsg-dictionary.php');
 
-        $filename="image Caption"; 
-        $document="https://file-example.s3-accelerate.amazonaws.com/documents/cv.pdf"; 
-        $api=$client->sendDocumentMessage($to,$filename,$document);
-        dump($api);
-
-        $audio="https://file-example.s3-accelerate.amazonaws.com/audio/2.mp3"; 
-        $api=$client->sendAudioMessage($to,$audio);
-        dump($api);
-
-        $audio="https://file-example.s3-accelerate.amazonaws.com/voice/oog_example.ogg"; 
-        $api=$client->sendVoiceMessage($to,$audio);
-        dump($api);
-
-        $video="https://file-example.s3-accelerate.amazonaws.com/video/test.mp4";
-        $caption="video Caption"; 
-        $api=$client->sendVideoMessage($to,$video,$caption);
-        dump($api);
-
-        $link="https://ultramsg.com"; 
-        $api=$client->sendLinkMessage($to,$link);
-        dump($api);
-
-        $contact="14000000001@c.us"; 
-        $api=$client->sendContactMessage($to,$contact);
-        dump($api);
-
-        $address="ABC company \n Sixth floor , office 38"; 
-        $lat="25.197197"; 
-        $lng="55.2721877"; 
-        $api=$client->sendLocationMessage($to,$address,$lat,$lng);
-        dump($api);
-
-        $vcard="BEGIN:VCARD
-        VERSION:3.0
-        N:lastname;firstname
-        FN:firstname lastname
-        TEL;TYPE=CELL;waid=14000000001:14000000002
-        NICKNAME:nickname
-        BDAY:01.01.1987
-        X-GENDER:M
-        NOTE:note
-        ADR;TYPE=home
-        ADR;TYPE=work
-        END:VCARD";
-        $vcard = preg_replace("/[\n\r]/", "\n", $vcard);
-        $api=$client->sendVcardMessage($to,$vcard);
-        dump($api);
-    }
-    public function webhook(Request $request){
-        $ultramsg_token="x2epn697nmzwnolg"; // Ultramsg.com token
-        $instance_id="instance15890"; // Ultramsg.com instance id
         $ultramsgDictionary = new ultramsgDictionary();
-        $client = new WhatsAppApi($ultramsg_token,$instance_id);
+        $this->client = new UltraMsg\WhatsAppApi($ultramsg_token, $instance_id);
+
+        //get the JSON body from the instance
         $json = file_get_contents('php://input');
         $decoded = json_decode($json, true);
         //write parsed JSON to the 'requests.log' for debugging
@@ -156,6 +98,8 @@ class WhatsappController extends Controller
             }
         }
     }
+
+
     public function welcome($to, $noWelcome = false)
     {
         $welcomeStr = ($noWelcome) ? "```📢 Incorrect command 📢 ```\nPlease type one of these *commands*:\n" : "welcome to ultramsg bot Demo \n";
@@ -176,6 +120,8 @@ class WhatsappController extends Controller
 
         );
     }
+
+    //convert Arabic/Persian numbers to English 
     public function convert($string)
     {
         $persian = ['۰', '۱', '۲', '۳', '۴', '۵', '۶', '۷', '۸', '۹'];
