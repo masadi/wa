489c50f01addc48cb613bb30833be4aba1427026
masadi <masadi.com@gmail.com>
2022-08-27 20:05:40 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -25,19 +25,32 @@ Route::get('/message', function() {
 
 Route::post('/message', function(Request $request) {
     // TODO: validate incoming params first!
-
-    $url = "https://messages-sandbox.nexmo.com/v0.1/messages";
-    $params = ["to" => ["type" => "whatsapp", "number" => $request->input('number')],
-        "from" => ["type" => "whatsapp", "number" => "14157386170"],
+    $NEXMO_API_KEY = '5d1a459c';
+    $NEXMO_API_SECRET = 'WhvXkp6TDDZfoKwN';
+    $headers = ["Authorization" => "Basic " . base64_encode($NEXMO_API_KEY . ":" . $NEXMO_API_SECRET)];
+    $url = "https://messages-sandbox.nexmo.com/v1/messages";
+    $params = [
+        "from" => "14157386102",
+        "to" => "6287864496339",
+        "message_type" => "text",
+        "text" => "This is a WhatsApp Message sent from the Messages API",
+        "channel" => "whatsapp"
+        /*
+        "to" => [
+            "type" => "whatsapp", 
+            "number" => $request->input('number')
+        ],
+        "from" => [
+            "type" => "whatsapp", 
+            "number" => "14157386102"
+        ],
         "message" => [
             "content" => [
                 "type" => "text",
                 "text" => "Hello from Vonage and Laravel :) Please reply to this message with a number between 1 and 100"
             ]
-        ]
+        ]*/
     ];
-    $headers = ["Authorization" => "Basic " . base64_encode(env('NEXMO_API_KEY') . ":" . env('NEXMO_API_SECRET'))];
-
     $client = new \GuzzleHttp\Client();
     $response = $client->request('POST', $url, ["headers" => $headers, "json" => $params]);
     $data = $response->getBody();
@@ -52,8 +65,9 @@ Route::post('/webhooks/status', function(Request $request) {
 });
 
 Route::post('/webhooks/inbound', function(Request $request) {
+    $NEXMO_API_KEY = '5d1a459c';
+    $NEXMO_API_SECRET = 'WhvXkp6TDDZfoKwN';
     $data = $request->all();
-
     $text = $data['message']['content']['text'];
     $number = intval($text);
     Log::Info($number);
@@ -64,7 +78,7 @@ Route::post('/webhooks/inbound', function(Request $request) {
         Log::Info($respond_number);
         $url = "https://messages-sandbox.nexmo.com/v0.1/messages";
         $params = ["to" => ["type" => "whatsapp", "number" => $data['from']['number']],
-            "from" => ["type" => "whatsapp", "number" => "14157386170"],
+            "from" => ["type" => "whatsapp", "number" => "14157386102"],
             "message" => [
                 "content" => [
                     "type" => "text",
@@ -72,7 +86,7 @@ Route::post('/webhooks/inbound', function(Request $request) {
                 ]
             ]
         ];
-        $headers = ["Authorization" => "Basic " . base64_encode(env('NEXMO_API_KEY') . ":" . env('NEXMO_API_SECRET'))];
+        $headers = ["Authorization" => "Basic " . base64_encode($NEXMO_API_KEY . ":" . $NEXMO_API_SECRET)];
 
         $client = new \GuzzleHttp\Client();
         $response = $client->request('POST', $url, ["headers" => $headers, "json" => $params]);
