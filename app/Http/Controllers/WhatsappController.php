f3e5a56dcad7461ddf72d799da205af762197501
masadi <masadi.com@gmail.com>
2022-08-27 20:53:07 +0700

    Update WhatsappController.php

--- app/Http/Controllers/WhatsappController.php
+++ app/Http/Controllers/WhatsappController.php
@@ -15,6 +15,7 @@ class WhatsappController extends Controller
         $to="6285231444789"; 
         $body="Hello world"; 
         $api=$client->sendChatMessage($to,$body);
+        dump($api);
 
         $image="https://file-example.s3-accelerate.amazonaws.com/images/test.jpg"; 
         $caption="image Caption"; 
@@ -22,13 +23,16 @@ class WhatsappController extends Controller
         $referenceId="SDK";
         $nocache=false; 
         $api=$client->sendImageMessage($to,$image,$caption,$priority,$referenceId,$nocache);
+        dump($api);
 
         $filename="image Caption"; 
         $document="https://file-example.s3-accelerate.amazonaws.com/documents/cv.pdf"; 
         $api=$client->sendDocumentMessage($to,$filename,$document);
+        dump($api);
 
         $audio="https://file-example.s3-accelerate.amazonaws.com/audio/2.mp3"; 
         $api=$client->sendAudioMessage($to,$audio);
+        dump($api);
 
         $audio="https://file-example.s3-accelerate.amazonaws.com/voice/oog_example.ogg"; 
         $api=$client->sendVoiceMessage($to,$audio);
@@ -37,17 +41,21 @@ class WhatsappController extends Controller
         $video="https://file-example.s3-accelerate.amazonaws.com/video/test.mp4";
         $caption="video Caption"; 
         $api=$client->sendVideoMessage($to,$video,$caption);
+        dump($api);
 
         $link="https://ultramsg.com"; 
         $api=$client->sendLinkMessage($to,$link);
+        dump($api);
 
         $contact="14000000001@c.us"; 
         $api=$client->sendContactMessage($to,$contact);
+        dump($api);
 
         $address="ABC company \n Sixth floor , office 38"; 
         $lat="25.197197"; 
         $lng="55.2721877"; 
         $api=$client->sendLocationMessage($to,$address,$lat,$lng);
+        dump($api);
 
         $vcard="BEGIN:VCARD
         VERSION:3.0
@@ -63,5 +71,6 @@ class WhatsappController extends Controller
         END:VCARD";
         $vcard = preg_replace("/[\n\r]/", "\n", $vcard);
         $api=$client->sendVcardMessage($to,$vcard);
+        dump($api);
     }
 }
