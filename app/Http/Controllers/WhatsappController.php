3531c460c91c9a1c59c81e76cef7d729dede9320
masadi <masadi.com@gmail.com>
2022-08-28 00:28:20 +0700

    Update WhatsappController.php

--- app/Http/Controllers/WhatsappController.php
+++ app/Http/Controllers/WhatsappController.php
@@ -76,6 +76,14 @@ class WhatsappController extends Controller
         \Log::info($api);
     }
     public function webhook(Request $request){
+        $data = file_get_contents("php://input");
+        $event = json_decode($data, true);
+        if(isset($event)){
+            //Here, you now have event and can process them how you like e.g Add to the database or generate a response
+            $file = 'log.txt';  
+            $data =json_encode($event)."\n";  
+            file_put_contents($file, $data, FILE_APPEND | LOCK_EX);
+        }
         \Log::info('new');
     }
     public function webhook_old(Request $request){
