e6b56f2269fcc9534358e9462275fa08f64a2238
masadi <masadi.com@gmail.com>
2022-08-27 20:25:23 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -71,13 +71,14 @@ Route::post('/webhooks/inbound', function(Request $request) {
     $text = $data['message']['content']['text'];
     $number = intval($text);
     Log::Info($number);
+    Log::Info($data);
     if($number > 0) {
         $random = rand(1, 8);
         Log::Info($random);
         $respond_number = $number * $random;
         Log::Info($respond_number);
         $url = "https://messages-sandbox.nexmo.com/v0.1/messages";
-        $params = ["to" => ["type" => "whatsapp", "number" => $data['from']['number']],
+        /*$params = ["to" => ["type" => "whatsapp", "number" => $data['from']['number']],
             "from" => ["type" => "whatsapp", "number" => "14157386102"],
             "message" => [
                 "content" => [
@@ -85,6 +86,13 @@ Route::post('/webhooks/inbound', function(Request $request) {
                     "text" => "The answer is " . $respond_number . ", we multiplied by " . $random . "."
                 ]
             ]
+        ];*/
+        $params = [
+            "from" => "14157386102",
+            "to" => "6287864496339",
+            "message_type" => "text",
+            "text" => "The answer is " . $respond_number . ", we multiplied by " . $random . ".",
+            "channel" => "whatsapp"
         ];
         $headers = ["Authorization" => "Basic " . base64_encode($NEXMO_API_KEY . ":" . $NEXMO_API_SECRET)];
 
